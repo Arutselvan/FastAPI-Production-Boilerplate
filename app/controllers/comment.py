a71b6f86fe7b6a5c84ea54db961f92aebac48c70
Arutselvan <arutselvan710@gmail.com>
2026-03-16 17:52:52 -0400

    task-15: Prevent comments on archived projects
    [contamination] [task-15] [python]
    - CommentController.add() checks project status before creating comment
    - Raises BadRequestException if project is archived

--- app/controllers/comment.py
+++ app/controllers/comment.py
@@ -1,15 +1,21 @@
 from app.models import Comment
-from app.repositories import CommentRepository
+from app.repositories import CommentRepository, ProjectRepository
 from core.controller import BaseController
 from core.database.transactional import Propagation, Transactional
+from core.exceptions import BadRequestException
 
 
 class CommentController(BaseController[Comment]):
     """Comment controller."""
 
-    def __init__(self, comment_repository: CommentRepository):
+    def __init__(
+        self,
+        comment_repository: CommentRepository,
+        project_repository: ProjectRepository,
+    ):
         super().__init__(model=Comment, repository=comment_repository)
         self.comment_repository = comment_repository
+        self.project_repository = project_repository
 
     async def get_by_project_id(self, project_id: int) -> list[Comment]:
         """Returns a list of comments based on project_id."""
@@ -22,6 +28,10 @@ class CommentController(BaseController[Comment]):
     @Transactional(propagation=Propagation.REQUIRED)
     async def add(self, body: str, author_id: int, project_id: int) -> Comment:
         """Adds a comment."""
+        project = await self.project_repository.get_by_id(project_id)
+        if project.status == "archived":
+            raise BadRequestException("Cannot add comments to an archived project")
+
         return await self.comment_repository.create(
             {
                 "body": body,
